9592f050527ef50b69fd263ab02cdd372c24505c
noah-chongsiriwatana <nschong@umd.edu>
2022-11-09 21:52:42 -0500

    Spatial separation algorithm added.

--- image_analysis/caiman_data_utils.py
+++ image_analysis/caiman_data_utils.py
@@ -16,12 +16,15 @@ class CaimanDataUtils:
         spatial_components = pd.DataFrame(np.vstack((indices[:]//dims[0], indices[:]%dims[0])).T, columns = ['x','y'])
         spatial_components['y'] = spatial_components['y'].apply(lambda x: dims[1]-x)
         traces = pd.DataFrame(np.transpose(estimates['F_dff']))
-        
+        separations = np.zeros((indptr.len()-1, indptr.len()-1))
         self.dims = dims
         self.indptr = indptr
         self.spatial_components = spatial_components
         self.traces = traces
         self.corrs = correlation_matrix(traces)
+        for (x, y), el in np.ndenumerate(separations):
+            separations[x][y] = self.get_spatial_separation(x, y)
+        self.separations = pd.DataFrame(separations)
 
     def get_spatial_component(self, idx):
         return self.spatial_components.iloc[self.indptr[idx]:self.indptr[idx+1],:]
@@ -37,7 +40,7 @@ class CaimanDataUtils:
         comp = pd.DataFrame(np.repeat(np.arange(0,self.indptr.len()-1,1),self.indptr.len()), columns=['cell1'])
         comp['cell2'] = np.tile(np.arange(0,self.indptr.len()-1,1),self.indptr.len()).tolist()
         comp['corr'] = comp.apply(lambda row: self.corrs[row['cell1']][row['cell2']], axis=1)
-        comp['distance'] = comp.apply(lambda row: row['cell1'], axis=1)
+        comp['distance'] = comp.apply(lambda row: self.separations[row['cell1']][row['cell2']], axis=1)
         
         return comp
         
