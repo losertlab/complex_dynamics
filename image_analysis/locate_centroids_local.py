57aa6aab87bc5f3c63a5a735fcdba75e9ce0e9db
Noah Sansern Chongsiriwatana <nschong@umd.edu>
2023-02-14 23:31:58 -0500

    Adds motion correction and cell segmentation.

--- image_analysis/locate_centroids_local.py
+++ image_analysis/locate_centroids_local.py
@@ -21,11 +21,42 @@ from caiman.summary_images import local_correlations_movie_offline
 
 def main():
     pass
+    cwd = os.getcwd().replace("C:", "/c").replace("\\", "/")
+    fnames = [os.path.relpath(sys.argv[1], cwd)]
     opts_dict = ast.literal_eval(sys.argv[2])
-    opts_dict["fnames"] = os.path.relpath(opts_dict["fnames"], os.getcwd().replace("C:", "/c").replace("\\", "/"))
+    opts_dict["fnames"] = fnames 
     
-    print(os.path.isfile(opts_dict["fnames"]))
-    #opts = params.CNMFParams(params_dict=opts_dict)
+    opts = params.CNMFParams(params_dict=opts_dict)
+    
+    if 'dview' in locals():
+        cm.stop_server(dview=dview)
+    c, dview, n_processes = cm.cluster.setup_cluster(
+        backend='local', n_processes=None, single_thread=False)
+
+    #mc = MotionCorrect(fnames, dview=dview, **opts.get_group('motion'))
+
+   # mc.motion_correct(save_movie=True)
+   # m_els = cm.load(mc.fname_tot_rig)
+    #border_to_0 = 0 if mc.border_nan == 'copy' else mc.border_to_0 
+
+   # fname_new = cm.save_memmap(mc.mmap_file, base_name='memmap_', order='C',
+    #    border_to_0=border_to_0, dview=dview) # exclude borders
+
+# now load the file
+    #Yr, dims, T = cm.load_memmap(fname_new)
+   # images = np.reshape(Yr.T, [T] + list(dims), order='F')
+
+   # cm.stop_server(dview=dview)
+   # c, dview, n_processes = cm.cluster.setup_cluster(
+   #     backend='local', n_processes=None, single_thread=False)
+
+    #cnm = cnmf.CNMF(n_processes, params=opts, dview=dview)
+    #cnm = cnm.fit(images)
+
+    cnm = cnmf.CNMF(n_processes, params=opts, dview=dview)
+    cnm.fit_file(motion_correct=True)
+
+    print(cnm)
 
 if __name__ == "__main__":
     main()
