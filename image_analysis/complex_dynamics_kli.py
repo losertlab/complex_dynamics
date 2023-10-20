dfb2aa793e736f08ae9f53ffac70d62657b4f084
kath3rine <not.katherine.li@gmail.com>
2023-10-20 13:07:47 -0400

    fake matrix

--- image_analysis/complex_dynamics_kli.py
+++ image_analysis/complex_dynamics_kli.py
@@ -1,16 +1,17 @@
 from caiman_data_utils import CaimanDataUtils
 import h5py
 import numpy as np
-import pandas as pd
+# import pandas as pd
 import matplotlib.pyplot as plt
+from tifffile import imsave
 
 
 PATH = "/Users/qianmin/research/data/locate_centroids.hdf5"
 FILE = h5py.File(PATH, 'r+')
 
 # creates 1 matrix (ie img for 1 neuron)
-def binarized_matrix_help(data):
-    fov = np.zeros((2048, 2048)) # empty (black) matrix of the dim of the img
+def binarized_matrix_help(data, xdim, ydim):
+    fov = np.zeros((ydim, xdim)) # empty (black) matrix of the dim of the img
 
     for i in range(0, data.shape[0]): # fill in neurons
         tmp = data.iloc[i]
@@ -18,16 +19,42 @@ def binarized_matrix_help(data):
     
     return fov
 
-def binarized_matrix(file):
+def binarized_matrix(file, xdim, ydim):
     data_obj = CaimanDataUtils(file) # caiman object
-    r = np.empty([data_obj.num_cells, 2048, 2048]) 
+    r = np.empty([data_obj.num_cells, ydim, xdim]) 
     
     for i in range(data_obj.num_cells): # appends binarized matrix for the i'th neuron
-        r[i] = binarized_matrix_help(data_obj.get_spatial_component(i))
+        r[i] = binarized_matrix_help(data_obj.get_spatial_component(i), ydim, xdim)
     
     return r
 
-def plot_binarized_matrix(data, a, b, c, d):
+def combine_matrices(a, b, c):
+    combined = np.zeros((a.shape[0], a.shape[1]))
+    for row in range(a.shape[0]):
+        for col in range(a.shape[1]):
+            if a[row, col] == 1 or b[row, col] == 1 or c[row, col] == 1:
+                combined[row, col] = 1
+    return combined
+
+
+# dummy matrices (to test binary -> tiff on nikon)
+def fake_binarized_matrix(xdim, ydim, idx):
+    r = np.zeros((ydim, xdim))
+
+    # white rectangle
+    for row in range(ydim):
+        for col in range(xdim):
+            if ydim < idx + 100 and ydim > idx - 100:
+                r[row, col] = 1
+
+    return r
+
+def plot_matrix(matrix, title):
+    plt.imshow(matrix, cmap="gray")
+    plt.title(title)
+    plt.show()
+
+def plot_binarized_matrices(data, a, b, c, d):
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
     fig.tight_layout()
     
@@ -53,5 +80,5 @@ def plot_binarized_matrix(data, a, b, c, d):
 
     plt.show()
 
-bin_mat = binarized_matrix(FILE)
-plot_binarized_matrix(bin_mat, 0, 100, 200, 270)
+# DRIVER CODE for testing if jobs can read binary tiff 
+mask1 = fake_binarized_matrix(2048, 2048, 500)
